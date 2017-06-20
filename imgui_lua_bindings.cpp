e12f1a958ae65ef8ca0bb5d6df4f0ae747aadca9
David Briscoe <idbrii@gmail.com>
2017-06-19 17:39:32 -0700

    Remove lState global
    Refactor entrypoints to accept lState instead of declaring a global that
    we need to provide.

--- imgui_lua_bindings.cpp
+++ imgui_lua_bindings.cpp
@@ -21,9 +21,6 @@ extern "C" {
 // if lua errors
 
 
-// define this global before you call RunString or LoadImGuiBindings
-lua_State* lState;
-
 #ifdef ENABLE_IM_LUA_END_STACK
 // Stack for imgui begin and end
 std::deque<int> endStack;
@@ -43,7 +40,7 @@ static void ImEndStack(int type);
 
 // Example lua run string function
 // returns NULL on success and error string on error
-const char * RunString(const char* szLua) {
+const char * RunString(lua_State* lState, const char* szLua) {
   if (!lState) {
     fprintf(stderr, "You didn't assign the global lState, either assign that or refactor LoadImguiBindings and RunString\n");
   }
@@ -511,7 +508,7 @@ void compat_luaL_setfuncs(lua_State* L, const luaL_Reg* l, int nup, const char* 
 #endif
 }
 
-void LoadImguiBindings() {
+void LoadImguiBindings(lua_State* lState) {
   if (!lState) {
     fprintf(stderr, "You didn't assign the global lState, either assign that or refactor LoadImguiBindings and RunString\n");
   }
